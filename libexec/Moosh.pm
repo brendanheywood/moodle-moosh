fa6d0b3c7bbfa49740a82f71ad22db9c83f65d9d
Brendan Heywood <brendan@catalyst-au.net>
2013-04-22 13:55:44 +1000

    got remote backup and copy done

--- libexec/Moosh.pm
+++ libexec/Moosh.pm
@@ -5,15 +5,17 @@ use Exporter;
 
 our @ISA= qw( Exporter );
 
-our @EXPORT = qw( find_moodle parse_config info );
+our @EXPORT = qw( find_moodle parse_config info remote_backup remote_cmd);
 
 
 sub info {
     use Term::ANSIColor;
     my ($good, $message) = @_;
 
-    print ''.($good ? color("green").'[OK]   '.color("reset")
-                    : color("red")  .'[FAIL] '.color("reset") );
+    print ''.($good >   0 ? (color("green")   .'[OK]    '.color("reset")) :
+              $good == -1 ? (color("blue")    .'[DEBUG] '.color("reset")) :
+                            (color("red")     .'[FAIL]  '.color("reset"))
+             );
     print $message;
 
 }
@@ -64,6 +66,65 @@ sub parse_config {
     info ($c, "Parsed $c config items\n");
     return %db;
 }
+
+sub remote_cmd {
+
+    my ($host, $cmd, $stderr) = @_;
+
+    my $remote_cmd = "ssh $host $cmd";
+    if ($stderr){
+        $remote_cmd .= " 2>&1";
+    }
+    my $result = `$remote_cmd`;
+    return $result;
+}
+
+
+sub remote_backup {
+    my ($remote_host, %cfg) = @_;
+
+    my ($file, $cmd) = get_backup_cmd(%cfg);
+
+    my $res = remote_cmd($remote_host, $cmd, 1);
+    if (length($res)){
+        info (0, "Remote backup on $remote_host to $file\n");
+        info (-1, "$cmd \n");
+        info (-1, "$res \n");
+        exit;
+    }
+    info (1, "Remote backup to $remote_host:$file\n");
+    return $file;
+}
+
+sub get_backup_cmd {
+    my (%db) = @_;
+
+	use Data::Dumper;
+	use Sys::Hostname;
+	use POSIX qw/strftime/;
+	my $host = hostname;
+	my $date = strftime('%d-%b-%Y-%H:%M',localtime);
+
+	#print Dumper (%db);
+
+    my $command;
+
+	my $backup_file = "/tmp/$host\_$db{dbtype}_$db{dbhost}_$db{dbname}_$date.sql";
+	if($db{dbtype} eq 'mysql' or $db{dbtype} eq 'mysqli'){
+		$command = "mysqldump --skip-lock-tables -u $db{dbuser} --password='$db{dbpass}' $db{dbname} -r $backup_file -h $db{dbhost}";
+	} elsif($db{dbtype} eq 'postgres7' || $db{dbtype} eq 'pgsql'){
+	# This isn't terribly secure but we're generally dealing with dev boxes so no-one else should be around
+	 	$command = "export PGPASSWORD=\"$db{dbpass}\"; pg_dump -Fc -O $db{dbname} -U $db{dbuser} -f $backup_file -h $db{dbhost}";
+	} else {
+		info (0, "Don't know how to backup type: $db{dbtype}\n");
+		exit;
+	}
+
+    return ($backup_file, $command);
+
+}
+
+
 
 
 1;
